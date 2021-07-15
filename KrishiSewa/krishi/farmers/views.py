fc5107cf20079a5342e292634864b1ccfa7a0694
Ursula1999 <57608065+Ursula1999@users.noreply.github.com>
2021-07-15 16:20:07 +0545

    Trail

--- KrishiSewa/krishi/farmers/views.py
+++ KrishiSewa/krishi/farmers/views.py
@@ -339,12 +339,31 @@ Soil testing part -----------------------------------------
 @login_required
 @farmers_only
 def test(request):
-    return render(request, 'farmers/index.html')
+    return render(request, 'farmers/Npktest.html')
+
+
+@login_required
+@farmers_only
+def getNPK_Prediction(N, P, K, temp, humidity, ph):
+    model = pickle.load(open('npk_model.sav', 'rb'))
+    scaler = pickle.load(open('scaler.sav', 'rb'))
+
+    prediction = model.predict(scaler.transform([
+        [N, P, K, temp, humidity, ph]
+    ]))
+
+    crops = {20: 'rice', 11: 'maize', 3: 'chickpea', 9: 'kidneybeans', 18: 'pigeonpeas', 13: 'mothbeans',
+             14: 'mungbean', 2: 'blackgram', 10: 'lentil', 19: 'pomegranate', 1: 'banana', 12: 'mango', 7: 'grapes',
+             21: 'watermelon', 15: 'muskmelon', 0: 'apple', 16: 'orange', 17: 'papaya', 4: 'coconut', 6: 'cotton',
+             8: 'jute', 5: 'coffee'}
+
+    return crops[prediction]
 
 
 @login_required
 @farmers_only
 def result(request):
+    if
     N = int(request.GET['N'])
     P = int(request.GET['P'])
     K = int(request.GET['K'])
